f1bf77348382d1605c33b6154e4ca370a8637af1
Bill Wang <805801378@qq.com>
2017-09-04 11:54:39 +0800

    basic

--- imgaug_basic.py
+++ imgaug_basic.py
@@ -1,4 +1,6 @@
 from imgaug import augmenters as iaa
+from PIL import Image
+from numpy import *
 
 seq = iaa.Sequential([
     iaa.Crop(px=(0, 16)), # crop images from each side by 0 to 16px (randomly chosen)
@@ -6,4 +8,9 @@ seq = iaa.Sequential([
     iaa.GaussianBlur(sigma=(0, 3.0)) # blur images with a sigma of 0 to 3.0
 ])
 
-images_aug = seq.augment_images(images)
+pil_img =Image.open('apple/apple_80.jpg')
+pil_img.show()
+img = array(pil_img)
+images_aug = seq.augment_images(img)
+pil_im2 = Image.fromarray(uint8(img))
+pil_im2.show()
